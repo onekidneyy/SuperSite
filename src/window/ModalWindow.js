826ca3a035d9edbda69aeb0ad583808b6e578863
OneKidneyy <rjball11@yahoo.com>
2024-06-21 13:44:44 -0400

    Version 1.24

--- src/window/ModalWindow.js
+++ src/window/ModalWindow.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Rnd } from 'react-rnd';
 import './WindowStyles.css';
 
 const ModalWindow = ({ children, onClose, title, index }) => {
     const [dimensions, setDimensions] = useState({ width: 500, height: 300 });
 
-    const calculateCenterPosition = useCallback(() => {
+    const calculateCenterPosition = () => {
         const offset = index * 30; // Change this value to adjust the offset between modals
         const x = (window.innerWidth - dimensions.width) / 2 + offset;
         const y = (window.innerHeight - dimensions.height) / 2 + offset;
         return { x, y };
-    }, [dimensions, index]);
+    };
 
     const [position, setPosition] = useState(calculateCenterPosition());
 
     useEffect(() => {
         setPosition(calculateCenterPosition());
-    }, [dimensions, calculateCenterPosition]);
+    }, [dimensions, index]);
 
     useEffect(() => {
         const handleResize = () => {
@@ -50,7 +50,7 @@ const ModalWindow = ({ children, onClose, title, index }) => {
                 <div className="title-bar">
                     <div className="title-bar-text">{title}</div>
                     <div className="title-bar-controls">
-                        <button aria-label="Close" className="close-button" onClick={onClose}>X</button>
+                        <button aria-label="Close" className="close-button" onClick={onClose} onTouchEnd={onClose}>X</button>
                     </div>
                 </div>
                 <div className="window-body">
